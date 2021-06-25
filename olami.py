1a1f2f208178cab99be732757f1283f3a8ce7d31
lekan <adeniyisaheed75@gmail.com>
2021-06-24 20:45:00 -0700

    Make code readable

--- olami.py
+++ olami.py
@@ -1,33 +1,38 @@
 
 
+
 ask_num = input("pick 2 digit numbers between 10 and 99: ")
 
-if      len(ask_num) > 2:
-        new_entry2 = input("error, input digit with 2 values: ")
-
+if  len(ask_num) > 2:
+        new_entry1 = input("sorry, input digit with 2 values: ")
+            
+        
 
 elif    len(ask_num) < 2:
-        new_entry2 = input("error, input 2 digit nos: ")
-
-
+        new_entry1 = input("sorry, input 2 digit nos: ")
+                
+                
 elif    ask_num.isalpha():
-        new_entry1 = input("error, input num not alpha: ")
-
-
+        new_entry1 = input("sorry, input num not alphabet: ")
+                
+                
 elif    int(ask_num) > 10 and int(ask_num) <100:
         first_num = int(str(ask_num)[0])
         second_num = int(str(ask_num)[1])
         final = first_num + second_num
-        if  (final >= 0  and final <= 9):
-            print(final," is correct. you win!")
-
-        else:print("two digits but you lose!")
-
-
-
+        if (final >= 0  and final <= 9):
+                print(final," is correct. you win!")
+                
+        else:
+                print("two digits but you lose!")
+                new_entry1 = ask_num
+                new_entry1 = input("sorry, input number again ")       
+                
 else:
-        while ask_num is not True: 
-            new_entry1 = input("error, keep trying")
+        new_entry1 = input("sorry, keep trying")
+        # else:
+        #     while ask_num is not True: 
+                
 
 # elif    ask_num == list(ask_num):
 #         sum_ofnumber =0
